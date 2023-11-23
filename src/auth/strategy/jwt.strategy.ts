110c31ff16e346c1876df06526c2a62fdfebf08d
e-ian <ianemma70@gmail.com>
2023-11-24 00:35:28 +0300

    fix import

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -3,7 +3,7 @@ import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { Prisma } from "@prisma/client";
 import { ExtractJwt, Strategy } from "passport-jwt";
-import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaService } from "../../prisma/prisma.service";
 
 
 @Injectable()
