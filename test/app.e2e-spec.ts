03f161ae2c90d592621f8ee0d3198af6dde04ec3
e-ian <ianemma70@gmail.com>
2023-11-28 20:43:09 +0300

    add tests for edit user

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,6 +4,7 @@ import { AppModule } from "../src/app.module";
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { PrismaService } from '.././src/prisma/prisma.service'
 import { AuthDto } from '.././src/auth/dto/auth.dto';
+import { EditUserDto } from 'src/user/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -142,7 +143,25 @@ describe('App e2e', () => {
       });
     });
 
-    describe('Edit user', () => {});
+    describe('Edit user', () => {
+
+      it('Should edit user', () => {
+        const dto: EditUserDto = {
+          firstName: 'Emmanuel',
+          email: 'ian@mtgh.com'
+        }
+        return pactum
+            .spec()
+            .patch(
+              '/users',
+            ).withHeaders({
+              Authorization: 'Bearer $S{userAt}',
+            }).withBody(dto)
+            .expectStatus(200)
+            .expectBodyContains(dto.firstName)
+            .expectBodyContains(dto.email)
+      });
+    });
   });
 
   describe('Bookmarks', () => {
@@ -152,8 +171,8 @@ describe('App e2e', () => {
 
     describe('Get bookmark by id', () => {});
 
-    describe('Edit bookmark', () => {});
+    describe('Edit bookmark by id', () => {});
 
-    describe('Delete bookmark', () => {});
+    describe('Delete bookmark by id', () => {});
   });
 });
