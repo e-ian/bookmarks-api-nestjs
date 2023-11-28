bfbbce22c009e427a24b332dfb573580bf2db0ea
e-ian <ianemma70@gmail.com>
2023-11-28 21:36:31 +0300

    add bookmark controller logic

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -7,34 +7,60 @@ import { CreateBookmarkDto, EditBookmarkDto } from './dto';
 @UseGuards(JwtGuard)
 @Controller('bookmarks')
 export class BookmarkController {
-    constructor(private bookmarkService: BookmarkService){}
+    constructor(private bookmarkService: BookmarkService,
+    ){}
 
     @Get()
     getBookmarks(
-        @GetUser('id') userId: number
-    ){}
+        @GetUser('id') userId: number){
+            return this.bookmarkService.getBookmarks(
+                userId,
+            );
+        }
 
     @Get(':id')
     getBookmarkById(
         @GetUser('id') userId: number,
         @Param('id', ParseIntPipe) bookmarkId: number
-    ){}
+    ){
+        return this.bookmarkService.getBookmarkById(
+            userId,
+            bookmarkId
+        )
+    }
 
     @Post()
     createBookmark(
         @GetUser('id') userId: number,
         @Body() dto: CreateBookmarkDto,
-    ){}
+    ){
+        return this.bookmarkService.createBookmark(
+            userId,
+            dto
+        );
+    }
 
-    @Patch()
+    @Patch('id')
     editBookmarkById(
         @GetUser('id') userId: number,
+        @Param('id', ParseIntPipe) bookmarkId: number,
         @Body() dto: EditBookmarkDto,
-    ){}
+    ){
+        return this.bookmarkService.editBookmarkById(
+            userId,
+            bookmarkId,
+            dto,
+        );
+    }
 
     @Delete('id')
     deleteBookmarkById(
         @GetUser('id') userId: number,
         @Param('id', ParseIntPipe) bookmarkId: number
-    ){}
+    ){
+        return this.bookmarkService.deleteBookmarkById(
+            userId,
+            bookmarkId
+        )
+    }
 }
